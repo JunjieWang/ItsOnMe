0387cece8b95ddac2950ee0caa860aad996a3209
Junjie Wang <junjie@uw.edu>
2014-02-18 01:16:05 -0800

    initial  setup

---
+++ src/com/devwang/itsonme/AddNew.java
@@ -0,0 +1,14 @@
+package com.devwang.itsonme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class AddNew extends Activity {
+	Button add;
+	
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.add);
+	}
+}
